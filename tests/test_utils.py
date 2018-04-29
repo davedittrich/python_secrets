0e6abbe0b1dcd422e5e48bd44a2f1ff744fbaad3
Dave Dittrich <dave.dittrich@gmail.com>
2018-04-28 17:52:22 -0700

    Add test for .find()

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -15,7 +15,10 @@ from python_secrets.utils import *
 class Test_Utils(unittest.TestCase):
 
     def setUp(self):
-        pass
+        self.lst = [
+               {'Variable': 'jenkins_admin_password', 'Type': 'password'},
+               {'Variable': 'ca_rootca_password', 'Type': 'password'},
+               ]
 
     def tearDown(self):
         pass
@@ -25,6 +28,12 @@ class Test_Utils(unittest.TestCase):
 
     def test_001_redact(self):
         assert redact("foo", True) == "REDACTED"
+
+    def test_002_find(self):
+        assert find(self.lst, 'Variable', 'ca_rootca_password') == 1
+
+    def test_003_find(self):
+        assert find(self.lst, 'Variable', 'something_not_there') is None
 
 
 if __name__ == '__main__':
