b1b044a2e3e526f8dc72d321eb21570e151ca889
Dave Dittrich <dave.dittrich@gmail.com>
2018-04-30 21:58:09 -0700

    Rename tests

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -23,16 +23,16 @@ class Test_Utils(unittest.TestCase):
     def tearDown(self):
         pass
 
-    def test_000_redact(self):
+    def test_redact_false(self):
         assert redact("foo", False) == "foo"
 
-    def test_001_redact(self):
+    def test_redact_true(self):
         assert redact("foo", True) == "REDACTED"
 
-    def test_002_find(self):
+    def test_find_present(self):
         assert find(self.lst, 'Variable', 'ca_rootca_password') == 1
 
-    def test_003_find(self):
+    def test_find_absent(self):
         assert find(self.lst, 'Variable', 'something_not_there') is None
 
 
