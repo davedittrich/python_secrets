4124e9b7528e6dc0553be22d775ed9042ae3dab4
Dave Dittrich <dave.dittrich@gmail.com>
2018-05-09 09:13:42 -0700

    pep8 fixes

--- python_secrets/__init__.py
+++ python_secrets/__init__.py
@@ -4,4 +4,4 @@ __author__ = 'Dave Dittrich'
 __email__ = 'dave.dittrich@gmail.com'
 __version__ = '0.7.2'
 
-__all__ = ['__author__','__email__','__version__']
+__all__ = ['__author__', '__email__', '__version__']
