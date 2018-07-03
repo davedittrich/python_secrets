a60756238c8c8d44d8e1a05d6752ade4d594ca28
Dave Dittrich <dave.dittrich@gmail.com>
2018-07-03 01:28:29 -0700

    Add argument checking function

--- python_secrets/utils.py
+++ python_secrets/utils.py
@@ -8,4 +8,12 @@ def find(lst, key, value):
 def redact(string, redact=False):
     return "REDACTED" if redact else string
 
+
+def require_options(options, *args):
+    missing = [arg for arg in args if getattr(options, arg) is None]
+    if missing:
+        raise RuntimeError('Missing options: %s' % ' '.join(missing))
+    return True
+
+
 # vim: set fileencoding=utf-8 ts=4 sw=4 tw=0 et :
