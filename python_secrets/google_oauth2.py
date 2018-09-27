6900afab215b5177d52908c079a5ec658397f30a
Dave Dittrich <dittrich@u.washington.edu>
2018-09-27 11:42:25 -0700

    Resolve pep8 finding

--- python_secrets/google_oauth2.py
+++ python_secrets/google_oauth2.py
@@ -267,12 +267,9 @@ class GoogleSMTP(object):
 
     def send_mail(self, fromaddr, toaddr, subject, message):
         self.access_token, self.expires_in = self.refresh_authorization()
-        # TODO(dittrich): probably don't want to use 'fromaddr' here...
         auth_string = self.generate_oauth2_string(base64_encode=True)
-
         # Note: version number is tracked with bumpversion (see "setup.cfg")
-        message = message + textwrap.dedent("""
-        
+        message = message + textwrap.dedent("""\n
         --
         Sent using python_secrets version 0.17.3
         https://pypi.org/project/python-secrets/
