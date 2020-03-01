dbe195f48e158e1dd1aa185ed74b9a3b7ca97109
Dave Dittrich <dave.dittrich@gmail.com>
2020-02-29 23:17:48 -0800

    Partially working 'secrets backup' (and 'restore')

--- psec/secrets.py
+++ psec/secrets.py
@@ -10,6 +10,8 @@ URL: https://python_secrets.readthedocs.org.
 import argparse
 import base64
 import binascii
+import contextlib
+import datetime
 import hashlib
 import json
 import logging
@@ -21,6 +23,7 @@ import re
 import secrets
 import stat
 import sys
+import tarfile
 import textwrap
 import uuid
 import yaml
@@ -148,6 +151,16 @@ def is_valid_environment(env_path, verbose_level=1):
         logger.warning('[!] environment directory {} '.format(env_path) +
                        'exists but is empty')
     return is_valid
+
+
+@contextlib.contextmanager
+def cd(path):
+    old_path = os.getcwd()
+    os.chdir(path)
+    try:
+        yield
+    finally:
+        os.chdir(old_path)
 
 
 class SecretsEnvironment(object):
@@ -937,6 +950,52 @@ def generate_random_base64(unique=False, size=DEFAULT_SIZE):
     return base64.b64encode(os.urandom(size)).decode('UTF-8')
 
 
+class SecretsBackup(Command):
+    """Back up just secrets and descriptions."""
+
+    LOG = logging.getLogger(__name__)
+
+    def get_parser(self, prog_name):
+        parser = super().get_parser(prog_name)
+        parser.formatter_class = argparse.RawDescriptionHelpFormatter
+        parser.epilog = textwrap.dedent("""
+            Creates a backup (``tar`` format) of the secrets.yml file
+            and all description files.
+            """)
+        return parser
+
+    def take_action(self, parsed_args):
+        self.LOG.debug('backup secrets')
+        secrets = self.app.secrets
+        secrets.requires_environment()
+        backups_dir = os.path.join(
+            secrets.environment_path(),
+            "backups")
+        if not os.path.exists(backups_dir):
+            os.mkdir(backups_dir, mode=0o700)
+        elif not os.path.isdir(backups_dir):
+            raise RuntimeError(f"{backups_dir} is not a directory")
+
+        # '2020-03-01T06:11:16.572992+00:00'
+        iso8601_string = datetime.datetime.utcnow().replace(
+                tzinfo=datetime.timezone.utc).isoformat()
+        backup_name = f"{secrets.environment()}_{iso8601_string}.tgz"
+        backup_path = os.path.join(backups_dir, backup_name)
+
+        # Change directory to allow relative paths in tar file,
+        # then force relative paths (there has to be a better way...
+        # just not right now.)
+        env_path = secrets.environment_path() + os.path.sep
+        with cd(env_path):
+            with tarfile.open(backup_path, "w:gz") as tf:
+                tf.add(
+                    secrets.secrets_file_path().replace(env_path, "", 1))
+                tf.add(
+                    secrets.descriptions_path().replace(env_path, "", 1))
+
+        self.LOG.info(f'created backup {backup_path}')
+
+
 class SecretsShow(Lister):
     """List the contents of the secrets file or definitions."""
 
@@ -1593,6 +1652,25 @@ class SecretsPath(Command):
         self.LOG.debug('returning secrets path')
         e = SecretsEnvironment(environment=parsed_args.environment)
         print(e.secrets_file_path())
+
+
+class SecretsRestore(Command):
+    """Restore secrets and descriptions from a backup file."""
+
+    LOG = logging.getLogger(__name__)
+
+    def get_parser(self, prog_name):
+        parser = super().get_parser(prog_name)
+        parser.formatter_class = argparse.RawDescriptionHelpFormatter
+        parser.epilog = textwrap.dedent("""
+            TODO(dittrich): Finish documenting command.
+            """)
+        return parser
+
+    def take_action(self, parsed_args):
+        self.LOG.debug('restore secrets')
+        self.app.secrets.requires_environment()
+        raise RuntimeError('Not implemented yet')
 
 
 # vim: set fileencoding=utf-8 ts=4 sw=4 tw=0 et :
