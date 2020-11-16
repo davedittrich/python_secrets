ae3ea7366c6bc87e27217d8b25650762f27635e7
Dave Dittrich <dave.dittrich@gmail.com>
2020-11-15 22:15:33 -0800

    Allow 'secrets set' to set by default from Options

--- psec/secrets/set.py
+++ psec/secrets/set.py
@@ -8,6 +8,7 @@ import textwrap
 
 from cliff.command import Command
 from psec.secrets import BOOLEAN_OPTIONS
+from psec.secrets import is_generable
 from psec.secrets import SecretsEnvironment
 from subprocess import run, PIPE  # nosec
 
@@ -21,6 +22,14 @@ class SecretsSet(Command):
         parser = super().get_parser(prog_name)
         parser.formatter_class = argparse.RawDescriptionHelpFormatter
         parser.add_argument(
+            '--undefined',
+            action='store_true',
+            dest='undefined',
+            default=False,
+            help="Set values for undefined variables (default: False)"
+        )
+        how = parser.add_mutually_exclusive_group(required=False)
+        how.add_argument(
             '--from-environment',
             metavar='<environment>',
             dest='from_environment',
@@ -28,12 +37,12 @@ class SecretsSet(Command):
             help="Environment from which to copy " +
                  "secret value(s) (default: None)"
         )
-        parser.add_argument(
-            '--undefined',
+        how.add_argument(
+            '--from-options',
             action='store_true',
-            dest='undefined',
+            dest='from_options',
             default=False,
-            help="Set values for undefined variables (default: False)"
+            help="Set from first available option (default: False)"
         )
         parser.add_argument('arg', nargs='*', default=None)
         parser.epilog = textwrap.dedent("""
@@ -92,22 +101,49 @@ class SecretsSet(Command):
 
     def take_action(self, parsed_args):
         self.LOG.debug('setting secrets')
-        if len(parsed_args.arg) == 0 and not parsed_args.undefined:
+        if (
+            len(parsed_args.arg) == 0
+            and not parsed_args.undefined
+            and not parsed_args.from_options
+        ):
             raise RuntimeError('no secrets specified to be set')
-        self.app.secrets.read_secrets_and_descriptions()
+        se = self.app.secrets
+        se.read_secrets_and_descriptions()
+        options = dict(se.Options)
+        # TODO(dittrich): Use Variable map like this elsewhere
+        variables = dict(se.Variable)
+        types = dict(se.Type)
         from_env = None
         if parsed_args.from_environment is not None:
             from_env = SecretsEnvironment(
                 environment=parsed_args.from_environment)
             from_env.read_secrets()
+            options = dict(from_env.secrets.Options)
+            variables = dict(from_env.secrets.Variable)
+            types = dict(from_env.secrets.Type)
+        args = (
+            list(variables.keys()) if not len(parsed_args.arg)
+            else parsed_args.arg
+        )
         if parsed_args.undefined:
-            args = [k for k, v in self.app.secrets.items()
+            # Downselect to just those currently undefined
+            args = [k for k, v in variables
                     if v in [None, '']]
-        else:
-            args = parsed_args.arg
+        if not len(args):
+            raise RuntimeError('[-] no secrets identified to be set')
         for arg in args:
             k, v, k_type = None, None, None
-            if '=' not in arg:
+            if parsed_args.from_options:
+                k, v, k_type = (
+                    arg,
+                    options.get(arg, '').split(',')[0],
+                    types.get(arg)
+                )
+                # Don't set from options if the type is generable
+                if is_generable(k_type):
+                    continue
+                v = None if v == '*' else v
+            elif '=' not in arg:
                 # No value was specified with the argument
                 k = arg
                 k_type = self.app.secrets.get_type(k)
@@ -172,9 +208,10 @@ class SecretsSet(Command):
                         v = p.stdout.decode('UTF-8').strip()
             # After all that, did we get a value?
             if v is None:
-                self.LOG.info(f'could not obtain value for "{k}"')
-            self.LOG.debug(f'setting variable "{k}"')
-            self.app.secrets.set_secret(k, v)
+                self.LOG.info(f'[-] could not obtain value for "{k}"')
+            else:
+                self.LOG.debug(f'[+] setting variable "{k}"')
+                self.app.secrets.set_secret(k, v)
 
 
 # vim: set fileencoding=utf-8 ts=4 sw=4 tw=0 et :
