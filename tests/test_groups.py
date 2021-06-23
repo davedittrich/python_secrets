87326697976f6362823e1b4e0cc805dea7165129
Dave Dittrich <dave.dittrich@gmail.com>
2021-06-23 06:28:15 -0700

    Add skeleton test for groups commands

---
+++ tests/test_groups.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+test_psec.groups
+----------------
+
+Tests for `psec.groups` module.
+"""
+
+import unittest
+import os
+import sys
+
+# from unittest.mock import patch
+
+HOST = 'example.com'
+HOME = os.path.expanduser('~')
+TESTENV = 'testing'
+SECRETS_SUBDIR = 'tsecrets'
+KEYS_SUBDIR = 'keys'
+
+
+def groups_dir(env=None, basedir=None):
+    if env is not None:
+        env_str = str(env)
+    else:
+        env = os.getenv('D2_ENVIRONMENT', None)
+        cwd = os.getcwd()
+        default_file = os.path.join(cwd, '.python_secrets_environment')
+        if os.path.exists(default_file):
+            with open(default_file, 'r') as f:
+                env_str = f.read().strip()
+        else:
+            env_str = os.path.basename(cwd)
+    basedir = os.getenv('D2_SECRETS_BASEDIR', None)
+    if basedir is None:
+        basedir = os.path.join(
+                HOME,
+                'secrets' if sys.platform.startswith('win') else '.secrets')
+    return os.path.join(basedir, env_str)
+
+
+# TODO(dittrich): Finish tests for groups
+
+if __name__ == '__main__':
+    sys.exit(unittest.main())
+
+# vim: set fileencoding=utf-8 ts=4 sw=4 tw=0 et :
