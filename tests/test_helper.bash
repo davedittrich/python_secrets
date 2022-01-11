26832a08c6bb4c4787f7ad379dd0f0631aa68362
Dave Dittrich <dave.dittrich@gmail.com>
2022-01-10 17:02:04 -0800

    Add environment cleanup function to test_helper

--- tests/test_helper.bash
+++ tests/test_helper.bash
@@ -4,6 +4,25 @@ source test-environment.bash
 export PYTHONPATH=$(pwd)
 export PSEC="python -m psec.__main__ --debug"
 
+trap "clean_bats" SIGINT
+
+function clean_bats() {
+    rm -f ${BATS_TEST_DIRNAME}/bats_*
+}
+
+# By default, cleans up the standard environment. Also use to
+# clean up any alternative environments created during testing
+# by specifying them as arguments when calling the function.
+
+function clean_environments() {
+    BASEDIR="${D2_SECRETS_BASEDIR:?warning - not set}"
+    ENVPATH="${BASEDIR}${D2_ENVIRONMENT:?warning - not set}"
+    for environment in $*
+    do
+        rm -rf ${BASEDIR}/${environment}
+    done
+}
+
 load 'libs/bats-support/load'
 load 'libs/bats-assert/load'
 
