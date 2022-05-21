7ffaee7cbce1ea6a9863eeadbd844186675a53e7
Dave Dittrich <dave.dittrich@gmail.com>
2022-05-21 15:40:50 -0700

    Resolve `import secrets` conflict

---
+++ psec/cli/utils/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+"""
+'utils' subcommands and related classes.
+
+  Author: Dave Dittrich <dave.dittrich@gmail.com>
+
+  URL: https://python_secrets.readthedocs.org.
+"""
+
+# vim: set fileencoding=utf-8 ts=4 sw=4 tw=0 et :
