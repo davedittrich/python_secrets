6f10b1567663eb32d95973c59f29423405d46cc7
Dave Dittrich <dave.dittrich@gmail.com>
2022-05-26 13:25:51 -0700

    Improve `secrets get` command logic and help

--- psec/cli/secrets/get.py
+++ psec/cli/secrets/get.py
@@ -12,11 +12,17 @@ from cliff.command import Command
 
 class SecretsGet(Command):
     """
-    Get value associated with a secret.
+    Gets the value associated with a secret and returns it on
+    standard output.  This is used for inline command substitution
+    in cases where a single value is needed::
 
-    To get a subset of secrets, specify them as arguments to this
-    command. If no secrets are specified, they are all returned.
-    """
+        $ echo "Jenkins admin password: $(psec secrets get jenkins_admin_password)"
+        Jenkins admin password: OZONE.negate.TIPTOP.ocean
+
+    To get values for more than one secret, use `secrets show`
+    with one of the formatting options allowing you to parse
+    the results or otherwise use them as a group.
+    """  # noqa
 
     logger = logging.getLogger(__name__)
 
@@ -31,7 +37,7 @@ class SecretsGet(Command):
         )
         parser.add_argument(
             'secret',
-            nargs='?',
+            nargs=1,
             default=None
         )
         return parser
@@ -42,7 +48,7 @@ class SecretsGet(Command):
         se.read_secrets_and_descriptions()
         if parsed_args.secret is not None:
             value = se.get_secret(
-                parsed_args.secret, allow_none=True)
+                parsed_args.secret.pop(), allow_none=True)
             if not parsed_args.content:
                 print(value)
             else:
